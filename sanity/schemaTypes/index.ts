63947dbf0374b9b6d087b29455dc974ea24444e2
daggermaster3000 <quillan.favey@gmail.com>
2025-07-23 08:24:10 +0200

    added embedded studio

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import galleryImage from './gallery'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [galleryImage],
+}
--- studio/schemaTypes/index.ts
+++
@@ -1,3 +0,0 @@
-import galleryImage from './gallery'
-
-export const schemaTypes = [galleryImage]
