8b26a3c495e0559bc2ce8cd96715a80db2615fce
daggermaster3000 <quillan.favey@gmail.com>
2025-08-29 16:05:54 +0200

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,18 +11,18 @@ const myFont = localFont({
 
 export const metadata: Metadata = {
   title: 'Bougainvillier',
-  description: 'Le site officiel d\'ACTS Taekwondo Crissier. Rejoignez-nous et apprenez les techniques du Taekwondo.',
+  description: '',
   openGraph: {
     title: 'Bougainvillier',
-    description: 'Le site officiel d\'ACTS Taekwondo Crissier.',
-    url: 'https://acts-crissier.ch',
-    siteName: 'ACTS Taekwondo',
+    description: '',
+    url: '',
+    siteName: 'Bougainvillier',
     images: [
       {
-        url: '	https://acts-crissier.ch/_next/image?url=%2Fphoto_accueil.jpg&w=750&q=75', // Must be an absolute URL
+        url: '', // Must be an absolute URL
         width: 1200,
         height: 630,
-        alt: 'ACTS Taekwondo Crissier Logo',
+        alt: '',
       },
     ],
     locale: 'fr_CH',
